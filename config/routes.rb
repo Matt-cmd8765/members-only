e59a279374d11753be972ee5a6bec2e5d92d25b6
Matt Marini <mattmarini8765@gmail.com>
2023-10-28 01:13:42 +1100

    Initial commit. confused about devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  devise_scope :user do
+    root 'users/sessions#new'
+    get 'login', to: 'devise/sessions#new'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/rails/info/routes' => 'routes'
+
 end
